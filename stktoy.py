0dbd36fa47a26d88726164ac4a4214733cecb33f
Paul Hansen <paul.c.hansen@gmail.com>
2017-12-31 17:58:52 -0800

    trying to make an installable package, and added gitignore

--- stktoy.py
+++ stktoy.py
@@ -1,12 +1,11 @@
-import numpy as np
-import stkcontrol
+from setuptools import setup
 
-for ii in xrange(20):
-    stkcontrol.note(2, ii*1.0, 0.5, 50 * (1 + ii/20.0), 0.5 + 0.4*np.sin(2*np.pi*ii/4.2))
-    stkcontrol.note(1, (ii+0.5)*1.0, 0.5, 50 * (1 + ii/20.0), 0.5 + 0.4*np.cos(2*np.pi*ii/4.2))
-
-stkcontrol.stop(10.0)
-
-stkcontrol.writeWav("out.wav", 44100)
-
-
+setup(name="stkcontrol",
+    version="0.1",
+    description="STK WAV writer",
+    url="https://github.com/plisdku/stkcontrol",
+    author="Paul Hansen",
+    author_email="paul.c.hansen@gmail.com",
+    license="MIT",
+    packages="stkcontrol",
+    zip_safe=False)
