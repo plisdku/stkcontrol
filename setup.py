c09d52683cebca6632ec0ed302de557ff10252e7
Paul Hansen <paul.c.hansen@gmail.com>
2017-12-31 18:01:16 -0800

    blew something away... oops

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
-# Do nothing
+from setuptools import setup
+
+setup(name="stkcontrol",
+    version="0.1",
+    description="STK WAV writer",
+    url="https://github.com/plisdku/stkcontrol",
+    author="Paul Hansen",
+    author_email="paul.c.hansen@gmail.com",
+    license="MIT",
+    packages="stkcontrol",
+    install_requires=[
+        "numpy"
+    ],
+    zip_safe=False)
--- stktoy.py
+++ stktoy.py
@@ -1,11 +1,5 @@
-from setuptools import setup
+import stkcontrol
 
-setup(name="stkcontrol",
-    version="0.1",
-    description="STK WAV writer",
-    url="https://github.com/plisdku/stkcontrol",
-    author="Paul Hansen",
-    author_email="paul.c.hansen@gmail.com",
-    license="MIT",
-    packages="stkcontrol",
-    zip_safe=False)
+stkcontrol
+
+print "dang"
